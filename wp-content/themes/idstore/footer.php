6f56e42a3b7f1c0c5e69e068949b213793e09024
arsood <arsood@gmail.com>
2014-03-18 12:43:50 -0700

    Initial Commit

---
+++ wp-content/themes/idstore/footer.php
@@ -0,0 +1,49 @@
+
+<div class="row-fluid solemade-footer">
+	<div class="span4 txt-center">
+    	<input class="mailing-list-input" type="text" placeholder="Join our mailing list" />
+        <button class="mailing-list-button">></button>
+    </div>
+    <div class="span4 txt-center footer-slogan">
+    	Sandals Made With Love
+    </div>
+    <div class="span4">
+    	Icons here
+    </div>
+</div>
+
+<div class="container-fluid gray-border-top">
+	<div class="container">
+    	<div class="row-fluid solemade-footer-links">
+        	<div class="span2">
+            	<a href="#">SHOP</a>
+            </div>
+            <div class="span2">
+            	<a href="#">RETURNS</a>
+            </div>
+            <div class="span2">
+            	<a href="#">FAQs</a>
+            </div>
+            <div class="span2">
+            	<a href="#">SIZE CHART</a>
+            </div>
+            <div class="span2">
+            	<a href="#">PRIVACY POLICY</a>
+            </div>
+            <div class="span2">
+            	<a href="#">CONTACT US</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+	/* Always have wp_footer() just before the closing </body>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to reference JavaScript files.
+	 */
+
+	wp_footer();
+?>
+</body>
+</html>
